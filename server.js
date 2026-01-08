f83213eca7d05471df25cca4419c635d96649b3c
Tan Zhi Shan <24035177@myrp.edu.sg>
2026-01-08 12:42:11 +0800

    id

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/allcharacters', async(req, res) => {
     }
 })
 
-app.post('/addcharacter/', async(req, res) => {
+app.post('/addcharacter', async(req, res) => {
     const { game_name, character_name, character_img } = req.body;
     try {
         let connection = await mysql.createConnection(dbConfig);
